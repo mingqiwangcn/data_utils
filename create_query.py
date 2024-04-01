f4ebbb44aaffa7d431e70e55c05be53817529482
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-03-31 23:12:04 -0500

    nyc question

--- create_query.py
+++ create_query.py
@@ -1,24 +1,34 @@
+import os
 import csv
 import uuid
 import json
 
 def main():
-    q_data = []
-    with open('../solo_work/data/chicago_open/query/test/test_questions.csv') as f:
+    out_file = '../data/nyc_open_100K/query/test/fusion_query.jsonl'
+    if os.path.isfile(out_file):
+        print(f'{out_file} already exists.')
+        return
+    f_o = open(out_file, 'w')
+    with open('../data/nyc_open_100K/query/test/example_questions.csv') as f:
         reader = csv.reader(f, delimiter=',')
+        q_count = 0
         for row, item in enumerate(reader):
-            q_data.append(item)
-
-    with open('../solo_work/data/chicago_open/query/test/fusion_query.jsonl', 'w') as f_o:
-        for row, q_item in enumerate(q_data):
+            if row == 0:
+                continue
+            question = item[2].strip()
+            if question == '':
+                 continue
+            q_count += 1
+            table_id = item[1].strip()
             out_item = {
-                'id':str(row + 1),
-                'question':q_item[0],
-                'table_id_lst':[q_item[1]],
+                'id':str(q_count),
+                'question':question,
+                'table_id_lst':[table_id],
                 'answers':['n/a'],
                 'ctxs':[{"title": "", "text": "This is a example passage."}]
             }
             f_o.write(json.dumps(out_item) + '\n')
+    f_o.close()
 
 if __name__ == '__main__':
     main()
